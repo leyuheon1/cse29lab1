2912fd85337b56b7c1e2c90fb164ceb54e7959b5
leyuheon1 <69151671+leyuheon1@users.noreply.github.com>
2024-10-01 16:30:25 +0000

    edited the code

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  char hello[] = "Hello!"; 
-  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello[] = "Yuheon Joh codespace"; 
+  char hello2[] = { 72, 26, 18, 88, 111, 33, 0 };
 
   puts(hello);
   puts(hello2);
